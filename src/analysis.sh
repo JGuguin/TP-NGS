5e8545a8a2bfde0b79be3435f1fbce0329216ed5
ThibaultLatrille <thibault.latrille@ens-lyon.org>
2018-10-24 13:35:32 +0200

    Création d'une boucle avec assemblage de toutes les briques

--- src/analysis.sh
+++ src/analysis.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-
-# Oupppsss, I'm so empty ... I want to be filled to actually perform the analysis
-
-cd /mnt/data
-
-# Polymorphism dataset
-
-# Téléchargement du dossier avec tout
-wget https://tinyurl.com/2018-tp-polymorphism
-unzip 2018-tp-polymorphism
-
-cd 2018-tp-polymorphism-part2/
+cd /mnt/data/
 mkdir 1000genomes
 cd 1000genomes/
 
-# Téléchargement des données .vcf
+# Téléchargement des données
+
+# Génotype des individus (.vcf)
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chr20_GRCh38.genotypes.20170504.vcf.gz
 gunzip ALL.chr20_GRCh38.genotypes.20170504.vcf.gz
-
-
-# Filtering out non-coding polymorphism
 
 # Téléchargement de la référence
 wget ftp://ftp.ensembl.org/pub/release-94/gtf/homo_sapiens/Homo_sapiens.GRCh38.94.chr.gtf.gz
@@ -35,32 +23,21 @@ bedtools merge -c 4 -o distinct -i Homo_sorted.bed > Homo_merge.bed
 bedtools intersect -header -wb -a ALL.chr20_GRCh38.genotypes.20170504.vcf -b Homo_merge.bed > Chr20_intersect.vcf
 # -wb garde la localisation pour quel transcrit on a fait l'intersection
 
-# Filtrage par population
-# On utilise le script extract_pop.py pour faire cela: permet de créer des fichiers individuels contenant la population d'intérêt
+# Classification des mutations synonymes,non-synonymes ou stop
+#Téléchargement des transcrits
+wget ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/cds/Homo_sapiens.GRCh38.cds.all.fa.gz
+gunzip Homo_sapiens.GRCh38.cds.all.fa.gz
+vcf_coding_polymorphism.py -f Homo_sapiens.GRCh38.cds.all.fa -g Homo_sapiens.GRCh38.94.chr.gtf -v Chr20_intersect.vcf
+
+# Filtrage par population + analyse de la variance
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel
-extract_pop.py -h
-# pour savoir comment utiliser le script
-POP="GBR"
-extract_pop.py -p integrated_call_samples_v3.20130502.ALL.panel -k ${POP}
-vcftools --vcf Chr20_intersect.vcf --keep GBR.txt --recode --out Chr20_GBR.vcf
-# J'ai extrait ici que les anglais
-# On peut développer une boucle car il y a de nombreuses populations et super-populations
-# Super populations
-for SUPER_POP in "EUR" "AFR" "EAS" "AMR" "SAS"
+# Fichier qui contient la population pour chaque individu
+for CLASS in "Stop" "Syn" "NonSyn"
 do
-extract_pop.py -p integrated_call_samples_v3.20130502.ALL.panel -k ${SUPER_POP}
-vcftools --vcf Chr20_intersect.vcf --keep ${SUPER_POP}.txt --recode --out Chr20_${SUPER_POP}
+    for POP in "EUR" "AFR" "EAS" "AMR" "SAS" "GBR" "FIN" "CHS" "PUR" "CDX" "JPT" "CLM" "IBS" "PEL" "PJL" "KHV" "LWK" "ACB" "GWD" "ESN" "BEB" "MSL" "MXL" "STU" "ITU" "CEU" "YRI" "CHB" "ASW" "TSI" "GIH"
+    do
+    extract_pop.py -p integrated_call_samples_v3.20130502.ALL.panel -k ${POP}
+    vcftools --vcf Chr20_intersect.${CLASS}.vcf --keep ${POP}.txt --recode --out ${CLASS}_${POP}
+    vcf_analysis.py -v ${CLASS}_${POP}.recode.vcf
+    done
 done
-
-# Populations
-for POP in "GBR" "FIN" "CHS" "PUR" "CDX" "JPT" "CLM" "IBS" "PEL" "PJL" "KHV" "LWK" "ACB" "GWD" "ESN" "BEB" "MSL" "MXL" "STU" "ITU" "CEU" "YRI" "CHB" "ASW" "TSI" "GIH"
-do
-extract_pop.py -p integrated_call_samples_v3.20130502.ALL.panel -k ${POP}
-vcftools --vcf Chr20_intersect.vcf --keep ${POP}.txt --recode --out Chr20_${POP}
-done
-
-# Analysis
-# Voir script vcf_analysis.py
-
-# Filter by synonymous, non-synonymous, stop
-# Voir script vcf_coding_polymorphism.py
