3a627cb7177cd6f67014d64cf1b365472efc0378
JGuguin <43372577+JGuguin@users.noreply.github.com>
2018-11-14 10:14:32 +0100

    Script complet pour l'analyse finale

--- src/analysis.sh
+++ src/analysis.sh
@@ -9,7 +9,7 @@ cd 1000genomes/
 wget ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/ALL.chr20_GRCh38.genotypes.20170504.vcf.gz
 gunzip ALL.chr20_GRCh38.genotypes.20170504.vcf.gz
 
-# Téléchargement de la référence
+# Téléchargement de la référence (.gtf)
 wget ftp://ftp.ensembl.org/pub/release-94/gtf/homo_sapiens/Homo_sapiens.GRCh38.94.chr.gtf.gz
 gunzip Homo_sapiens.GRCh38.94.chr.gtf.gz
 
@@ -17,14 +17,14 @@ gunzip Homo_sapiens.GRCh38.94.chr.gtf.gz
 gtf_to_bed.py -g Homo_sapiens.GRCh38.94.chr.gtf
 # [-h] optionnel
 
-# Garder les exons en un seul exemplaire
+# Garder les exons en un seul exemplaire car certains exons sont présents dans plusieurs transcrits du fait d'épissage alternatif et cela peut induire un biais, dans le sens où les SNPs présents dans ces exons auront un poids plus important que les SNP situés dans un exon participant à plusieurs transcrits
 bedtools sort -i Homo_sapiens.GRCh38.94.chr.bed > Homo_sorted.bed
 bedtools merge -c 4 -o distinct -i Homo_sorted.bed > Homo_merge.bed
 bedtools intersect -header -wb -a ALL.chr20_GRCh38.genotypes.20170504.vcf -b Homo_merge.bed > Chr20_intersect.vcf
 # -wb garde la localisation pour quel transcrit on a fait l'intersection
 
-# Classification des mutations synonymes,non-synonymes ou stop
-#Téléchargement des transcrits
+# Classification des mutations synonymes, non-synonymes ou stop
+# Téléchargement des transcrits
 wget ftp://ftp.ensembl.org/pub/release-94/fasta/homo_sapiens/cds/Homo_sapiens.GRCh38.cds.all.fa.gz
 gunzip Homo_sapiens.GRCh38.cds.all.fa.gz
 vcf_coding_polymorphism.py -f Homo_sapiens.GRCh38.cds.all.fa -g Homo_sapiens.GRCh38.94.chr.gtf -v Chr20_intersect.vcf
@@ -45,3 +45,5 @@ for POP in "EUR" "AFR" "EAS" "AMR" "SAS" "GBR" "FIN" "CHS" "PUR" "CDX" "JPT" "CL
     do
     vcf_meta_analysis.py -o Stop_${POP}.recode.vcf -s Syn_${POP}.recode.vcf -n NonSyn_${POP}.recode.vcf -c 200
 done
+
+# Au final, on obtient des graphes indiquant la distribution de la variance pour les mutations synonymes et non-synonymes, ainsi que le ratio des variances pour les mutations LoF. On peut ainsi comparer ces ratios (LoF vs synonymes) afin de savoir à quel type d'interaction sont soumises les LoF.
